1a99581cf056872f0a66509f65f549a5616dd674
Mumtaz Aziz <git@mumtazaziz.com>
2025-02-15 13:33:23 +0700

    Update robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -3,7 +3,14 @@ import type { MetadataRoute } from "next/types";
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
-      allow: ["/$", "/_next/static/"],
+      allow: [
+        "/$",
+        "/_next/image/", 
+        "/_next/static/", 
+        "/favicon.ico$", 
+        "/robots.txt$", 
+        "/sitemap.xml$",
+      ],
       disallow: "/",
     },
     sitemap: "https://www.mumtazaziz.com/sitemap.xml",
